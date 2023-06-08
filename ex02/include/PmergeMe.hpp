df1b83eee548ee5a562760ed0282a6c781c771b1
Guillaume Baumgartner <gbaumgar@k0r2p13.42mulhouse.fr>
2023-06-08 12:39:10 +0200

    done

--- ex02/include/PmergeMe.hpp
+++ ex02/include/PmergeMe.hpp
@@ -3,10 +3,14 @@
 
 #include <vector>
 #include <list>
+#include <deque>
+#include <iostream>
+#include <algorithm>
 
 class PmergeMe {
 	std::vector<unsigned int> _vector;
 	std::list<unsigned int> _list;
+	std::deque<unsigned int> _deque;
 public:
 	PmergeMe();
 	PmergeMe(int i, char **values);
@@ -21,6 +25,26 @@ public:
 	std::list<unsigned int> sort_list(
 		std::list<unsigned int>::iterator begin,
 		std::list<unsigned int>::iterator end);
+
+	// template<class It, class T>
+	// T sort(It b, It e)
+	// {
+	// 	T t;
+	// 	if (e - b > 4) {
+	// 		T l = sort(b, b + (e - b) / 2);
+	// 		T r = sort(b + (e - b) / 2, e);
+	// 		//merge
+	// 	} else {
+	// 		for (It it = b, it != e, it++) {
+	// 			It jt = t.begin();
+	// 			while (jt != t.end() && *jt < *it)
+	// 				jt++;
+	// 			t.insert(jt, *it);
+	// 		}
+	// 	}
+	// 	return t;
+	// }
 };
+
 
 #endif
--- ex02/src/PMergeMe.cpp
+++ ex02/src/PMergeMe.cpp
@@ -11,6 +11,10 @@ PmergeMe::~PmergeMe() {}
 PmergeMe::PmergeMe(int i, char **values) {
 	for (int j = 0; j < i; j++) {
 		try {
+			std::string str(values[j]);
+			for (unsigned int i = 0; i < str.length(); i++)
+				if (!isdigit(str[i]))
+					throw "";
 			if (std::stoi(values[j]) < 0)
 				throw "";
 		} catch (...) {
@@ -35,52 +39,89 @@ PmergeMe::PmergeMe() {}
 
 std::vector<unsigned int> PmergeMe::sort_vector(std::vector<unsigned int>::iterator begin, std::vector<unsigned int>::iterator end) {
 	std::vector<unsigned int> result;
-	if ((end - begin) > 1) {
+	if ((end - begin) > 4) {
 		std::vector<unsigned int> a = sort_vector(begin, begin + (end - begin) / 2);
 		std::vector<unsigned int> b = sort_vector(begin + (end - begin) / 2, end);
-		std::merge(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(result));
+		std::vector<unsigned int>::iterator ita = a.begin();
+		std::vector<unsigned int>::iterator itb = b.begin();
+		while (ita != a.end() && itb != b.end()) {
+			if (*ita < *itb)
+				result.push_back(*ita++);
+			else
+				result.push_back(*itb++);
+		}
+		if (ita == a.end())
+			while (itb != b.end())
+				result.push_back(*itb++);
+		if (itb == b.end())
+			while (ita != a.end())
+				result.push_back(*ita++);
 	}
-	else
-		result.push_back(*begin);
+	else {
+		for (std::vector<unsigned int>::iterator it = begin; it != end; it++) {
+			std::vector<unsigned int>::iterator jt = result.begin();
+			while (jt != result.end() && *jt < *it)
+				jt++;
+			result.insert(jt, *it);
+		}
+	}
 	return result;
 }
 
 std::list<unsigned int> PmergeMe::sort_list(std::list<unsigned int>::iterator begin, std::list<unsigned int>::iterator end) {
 	std::list<unsigned int> result;
-	if (std::distance(begin, end) > 1) {
+	if (std::distance(begin, end) > 4) {
 		std::list<unsigned int>::iterator mid = begin;
 		std::advance(mid, std::distance(begin, end) / 2);
 		std::list<unsigned int> a = sort_list(begin, mid);
 		std::list<unsigned int> b = sort_list(mid, end);
-		a.merge(b);
-		return a;
+		std::list<unsigned int>::iterator ita = a.begin();
+		std::list<unsigned int>::iterator itb = b.begin();
+		while (ita != a.end() && itb != b.end()) {
+			if (*ita < *itb)
+				result.push_back(*ita++);
+			else
+				result.push_back(*itb++);
+		}
+		if (ita == a.end())
+			while (itb != b.end())
+				result.push_back(*itb++);
+		if (itb == b.end())
+			while (ita != a.end())
+				result.push_back(*ita++);
 	}
 	else {
-		result.push_back(*begin);
-		return result;
+		for (std::list<unsigned int>::iterator it = begin; it != end; it++) {
+			std::list<unsigned int>::iterator jt = result.begin();
+			while (jt != result.end() && *jt < *it)
+				jt++;
+			result.insert(jt, *it);
+		}
 	}
+	return result;
 }
 
 void PmergeMe::pmergeme() {
-	std::vector<unsigned int> vec;
-	std::list<unsigned int> lst;
 	std::chrono::high_resolution_clock::time_point vstart = std::chrono::high_resolution_clock::now();
-	vec = sort_vector(_vector.begin(), _vector.end());
+	std::vector<unsigned int> vec = sort_vector(_vector.begin(), _vector.end());
 	std::chrono::high_resolution_clock::time_point vend = std::chrono::high_resolution_clock::now();
 	std::chrono::high_resolution_clock::time_point lstart = std::chrono::high_resolution_clock::now();
-	lst = sort_list(_list.begin(), _list.end());
+	std::list<unsigned int> lst = sort_list(_list.begin(), _list.end());
 	std::chrono::high_resolution_clock::time_point lend = std::chrono::high_resolution_clock::now();
 	std::cout << std::setw(10) << std::left << "Before: ";
 	for (std::vector<unsigned int>::iterator it = _vector.begin(); it < _vector.end(); it++)
 		std::cout << *it << ' ';
 	std::cout << std::endl;
-	std::cout << std::setw(10) << std::left << "After: ";
+	std::cout << std::setw(10) << std::left << "Vector: ";
 	for (std::vector<unsigned int>::iterator it = vec.begin(); it < vec.end(); it++)
+		std::cout << *it << ' ';
+	std::cout << std::endl;
+	std::cout << std::setw(10) << std::left << "List: ";
+	for (std::list<unsigned int>::iterator it = lst.begin(); it != lst.end(); it++)
 		std::cout << *it << ' ';
 	std::cout << std::endl;
 	std::cout << std::setw(36) << "Time to process using std::vector : ";
 	std::cout << std::chrono::duration_cast<std::chrono::microseconds>(vend - vstart).count() << " us" << std::endl;
 	std::cout << std::setw(36) << "Time to process using std::list : ";
 	std::cout << std::chrono::duration_cast<std::chrono::microseconds>(lend - lstart).count() << " us" << std::endl;
-
 }
